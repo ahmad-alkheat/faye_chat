1ac0a84e538849556eabc29633d14c43ea59c766
Ahmad <wisamfaithful@gmail.com>
2015-04-19 18:27:38 -0400

    user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
  get '/login' => 'sessions#new', as: :login
- post '/login' => 'sessions#create', as: :login
+ post '/login' => 'sessions#create', as: :login2
 
 end
